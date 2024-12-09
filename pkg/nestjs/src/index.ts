22fb1dc443a26ecba1f98fd645e8325a38a766e8
Mark Wecke <wecke@neoskop.de>
2024-12-09 09:56:00 +0100

    feat(nestjs): add missing exports

---
+++ pkg/nestjs/src/index.ts
@@ -0,0 +1,6 @@
+export * from './a38.module';
+export * from './guard';
+export * from './hrbac.service';
+export * from './loader';
+export * from './tokens';
+export * from './types';
--- pkg/nestjs/src/tokens.ts
+++ pkg/nestjs/src/tokens.ts
@@ -1,1 +1,3 @@
-export const DEFAULT_ROLE = 'A38_DEFAULT_ROLE';
+import type { InjectionToken } from '@nestjs/common';
+
+export const DEFAULT_ROLE: InjectionToken<string> = 'A38_DEFAULT_ROLE';
